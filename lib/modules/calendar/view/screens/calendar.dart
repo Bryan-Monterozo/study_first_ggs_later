c049501406c26ec4ba807aff8970673391dd0407
redlagoy <lagoyreden@gmail.com>
2023-10-06 22:09:43 +0800

    changed api

--- lib/modules/calendar/view/screens/calendar.dart
+++ lib/modules/calendar/view/screens/calendar.dart
@@ -1,62 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
 import 'package:study_first_ggs_later/modules/shared/nav_bar.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class StudyCalendar extends StatelessWidget {
+class StudyCalendar extends StatefulWidget {
   const StudyCalendar({super.key});
 
   @override
+  State<StudyCalendar> createState() => _StudyCalendarState();
+}
+
+class _StudyCalendarState extends State<StudyCalendar> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const NavDrawer(),
-        appBar: SharedAppBar(
-          titlePic: titlePic(context),
-          withPic: withPic(context),
+      drawer: const NavDrawer(),
+      appBar: SharedAppBar(
+        titlePic: titlePic(context),
+        withPic: withPic(context),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Calendar',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            TableCalendar(
+              focusedDay: DateTime.now(),
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              // selectedDayPredicate: (day) {
+              //   return isSameDay(_selectedDay, day);
+              // },
+              // onDaySelected: (selectedDay, focusedDay) {
+              //   setState(() {
+              //     _selectedDay = selectedDay;
+              //     _focusedDay = focusedDay; // update `_focusedDay` here as well
+              //   });
+              // },
+            ),
+          ],
         ),
-        body: Center(
-          /*child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                  'Calendar',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-            ],
-          ),
-          ),*/
-          child: SfCalendar(
-            view: CalendarView.month,
-            firstDayOfWeek: 1,
-            dataSource: MeetingDataSource(getAppointments()),
-          ),
-        )
-      );
-  }
-}
-
-List<Appointment> getAppointments() {
-  List<Appointment> meetings = <Appointment>[];
-  final DateTime today = DateTime.now();
-  final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0);
-  final DateTime endTime = startTime.add(const Duration(hours: 2));
-
-  meetings.add(Appointment(
-    startTime: startTime,
-    endTime: endTime,
-    subject: 'Conference',
-    color: Colors.blue
-  ));
-
-  return meetings;
-}
-
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Appointment> source) {
-    appointments = source;
+      ),
+    );
   }
 }
