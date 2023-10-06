7fb01c4ebf27862bd355772bac1bc8ed3c7d6bb6
redlagoy <lagoyreden@gmail.com>
2023-10-06 13:26:48 +0800

    Calendar starting

--- lib/modules/calendar/view/screens/calendar.dart
+++ lib/modules/calendar/view/screens/calendar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
 import 'package:study_first_ggs_later/modules/shared/nav_bar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class StudyCalendar extends StatelessWidget {
   const StudyCalendar({super.key});
@@ -13,8 +14,8 @@ class StudyCalendar extends StatelessWidget {
           titlePic: titlePic(context),
           withPic: withPic(context),
         ),
-        body: const Center(
-          child: Column(
+        body: Center(
+          /*child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -27,7 +28,35 @@ class StudyCalendar extends StatelessWidget {
                 ),
             ],
           ),
+          ),*/
+          child: SfCalendar(
+            view: CalendarView.month,
+            firstDayOfWeek: 7,
+            dataSource: MeetingDataSource(getAppointments()),
           ),
-        );
+        )
+      );
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+    startTime: startTime,
+    endTime: endTime,
+    subject: 'Conference',
+    color: Colors.blue
+  ));
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
