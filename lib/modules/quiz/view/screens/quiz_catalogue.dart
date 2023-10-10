b4870d6c23d6ab33d4293cb498f246cd043dd331
Bryan-Monterozo <bmonterozo0406@gmail.com>
2023-10-11 02:45:52 +0800

    Quiz: added getx, toggle textformfield

--- lib/modules/quiz/view/screens/quiz_catalogue.dart
+++ lib/modules/quiz/view/screens/quiz_catalogue.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:study_first_ggs_later/modules/quiz/view/screens/quiz_add_question.dart';
+import 'package:study_first_ggs_later/modules/quiz/view/screens/quiz_create.dart';
 import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
 
 class QuizCatalogue extends StatelessWidget {
@@ -17,6 +19,7 @@ class QuizCatalogue extends StatelessWidget {
           ),
           IconButton(
             onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => QuizCreate()));
             },
             icon: const Icon(Icons.add),
           ),
--- lib/modules/quiz/view/screens/quiz_create.dart
+++ lib/modules/quiz/view/screens/quiz_create.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:study_first_ggs_later/modules/quiz/controller/quiz_get_controller.dart';
+import 'package:study_first_ggs_later/modules/quiz/model/quiz_model.dart';
+import 'package:study_first_ggs_later/modules/quiz/view/screens/quiz_add_question.dart';
 import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
-import 'package:study_first_ggs_later/modules/quiz/services/quiz_catalogue_collection.dart';
+// import 'package:study_first_ggs_later/modules/quiz/services/quiz_catalogue_collection.dart';
 
-class QuizCreate extends StatefulWidget {
-  const QuizCreate({super.key});
+class QuizCreate extends StatelessWidget {
+  final QuizController quizController = Get.put(QuizController());
+  final QuestionModel? questionModel;
+  final quizId;
 
-  @override
-  State<QuizCreate> createState() => _QuizCreateState();
-}
-
-class _QuizCreateState extends State<QuizCreate> {
-
-  String quizTitle = '';
-  String quizDesc = '';
+  QuizCreate({super.key, this.questionModel, this.quizId,});
 
   @override
   Widget build(BuildContext context) {
+
+    String quizTitle = '';
+    String quizDesc = '';
+
     return Scaffold(
       appBar: SharedAppBar(
         leading: leadingBack(context),
-        title: 'Create a Deck',
+        title: 'Create a Quiz',
         actions: [
           ElevatedButton(
             style: ElevatedButton.styleFrom(
@@ -37,41 +40,68 @@ class _QuizCreateState extends State<QuizCreate> {
           IconButton(
             icon: const Icon(Icons.save),
             onPressed: () {
-              QuizCatDB().addQuizToDB(
-                quizTitle: quizTitle, 
-                quizDesc: quizDesc,);
-              Navigator.pop(context);
+              // QuizCatDB().addQuizToDB(
+              //   quizTitle: quizTitle,
+              //   quizDesc: quizDesc,);
+              // Navigator.pop(context);
             },
           ),
         ],
       ),
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(border: Border.all()),
-              child: TextField(
-                maxLength: 60,
-                onChanged: (value) {
-                  quizTitle = value;
-                },
-                decoration: const InputDecoration(hintText: 'Deck Name'),
-              ),
+      body: Builder(builder: (context) {
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Obx(
+            () => Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: TextFormField(
+                    maxLength: 60,
+                    validator: (value) => value!.isEmpty ? 'Enter a title' : null,
+                    initialValue: quizTitle,
+                    onChanged: (value) {
+                      quizTitle = value;
+                    },
+                    decoration: const InputDecoration(hintText: 'Quiz Title'),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: TextFormField(
+                    maxLength: 60,
+                    validator: (value) => value!.isEmpty ? 'Enter a title' : null,
+                    initialValue: quizDesc,
+                    onChanged: (value) {
+                      quizDesc = value;
+                    },
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                ),
+                  quizController.hasQuestionField.value == false
+                    ? Container(
+                        margin: const EdgeInsets.all(30),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {
+                                    quizController.removeQuestionButton();
+                                    debugPrint('${quizController.hasQuestionField.value}');
+                                  },
+                                  child: const Text('Add Question')),
+                            ]),
+                      )
+                    : Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(10),
+                        child: QuizAddQuestion(questionModel: questionModel, quizId: quizId,)
+                      ),
+              ],
             ),
-            Container(
-              decoration: BoxDecoration(border: Border.all()),
-              child: TextField(
-                maxLength: 60,
-                onChanged: (value) {
-                  quizDesc = value;
-                },
-                decoration: const InputDecoration(hintText: 'Description'),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
