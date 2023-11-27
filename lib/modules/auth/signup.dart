bf5c064bd8640ab810c2154da4e74ce58919d2de
niyuhnuh <nianiaandrea@gmail.com>
2023-11-27 15:24:11 +0800

    Initial placeholders with footer for sign up

--- lib/modules/auth/signup.dart
+++ lib/modules/auth/signup.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:study_first_ggs_later/modules/auth/constants_login.dart';
-import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
-import 'package:study_first_ggs_later/modules/shared/nav_bar.dart';
+//import 'package:study_first_ggs_later/modules/auth/constants_login.dart';
+//import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
+//import 'package:study_first_ggs_later/modules/shared/nav_bar.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -13,13 +13,13 @@ class SignUp extends StatelessWidget {
     return SafeArea(
       child:(Scaffold(
         body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 50.0),
           child: Column(
             children:[
               Text('Image ng SF logo dito'),
               Text('Start Your Journey!'),
-              Text('Register today and be part of the community.'),
+              Text('Register today and be part of the community.'), //header end
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -34,6 +34,7 @@ class SignUp extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                         label: Text('First Name'),
+                        prefixIcon: Icon(Icons.person),
                         border: OutlineInputBorder(),
                         ),
                     ),
@@ -66,11 +67,37 @@ class SignUp extends StatelessWidget {
                       width:double.infinity,
                       child:ElevatedButton(
                         onPressed: (){},
-                        child: Text('Sign Up')))
+                        child: Text('Sign Up'))) //sign up form ends
                   ],
                 )
-              )
+              ),
+              SizedBox(height: 30.0),
+              Column(
+                children: [
+                    Text('OR'),
+                    SizedBox(height: 30.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton.icon(
+                        onPressed: (){}, 
+                        icon: Icon(Icons.public),
+                        label: Text('Sign Up with Google'),)
+              ),
+              TextButton(
+                onPressed: (){}, 
+                child: Text.rich(
+                  TextSpan(
+                    children: const [
+                      TextSpan(text: 'Already have an account?'),
+                      TextSpan(text: ' Log In')
+                    ]
+                  )
+                ))
+              
+              
+              ]),
             ],
+          
           
         ),
         
