5c3b1b4cb95bba2caee072b736241b8c809e0b0c
niyuhnuh <nianiaandrea@gmail.com>
2023-12-03 14:17:17 +0800

    Minor changes and form validation

--- lib/modules/auth/view/sceens/login.dart
+++ lib/modules/auth/view/sceens/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:study_first_ggs_later/modules/auth/controller/auth_get_controller.dart';
@@ -5,9 +7,18 @@ import 'package:study_first_ggs_later/modules/auth/view/sceens/signup.dart';
 //import 'package:study_first_ggs_later/modules/auth/constants_login.dart';
 //import 'package:study_first_ggs_later/modules/shared/app_bar.dart';
 
-
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formfield = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool passwordToggle = true;
 
   @override
   Widget build(BuildContext context) {
@@ -43,51 +54,64 @@ class Login extends StatelessWidget {
                             fontSize: 20,
                           )),
                       Form(
+                          key: _formfield,
                           child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                  prefixIcon: Icon(Icons.email),
-                                  labelText: 'E-mail',
-                                  hintText: 'E-mail',
-                                  border: OutlineInputBorder()),
-                              controller: authController.emailController,
-                            ),
-                            const SizedBox(height: 30),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                  prefixIcon: Icon(Icons.fingerprint),
-                                  labelText: 'Password',
-                                  hintText: 'Password',
-                                  border: OutlineInputBorder(),
-                                  suffixIcon: IconButton(
-                                    onPressed: null,
-                                    icon: Icon(Icons.remove_red_eye_sharp),
-                                  )),
-                              controller: authController.passwordController,
-                            ),
-                            const SizedBox(height: 30),
-                            const Align(
-                                alignment: Alignment.centerRight,
-                                child: TextButton(
-                                    onPressed: null,
-                                    child: Text('Forgot Password?'))),
-                            ElevatedButton( //ADMIN AUTH
-                              onPressed: () {
-                                if (authController.authEmail == 'ad' &&
-                                    authController.authPassword == 'ad') {
-                                    authController.adminLogin();
-                                  }
-                              },
-                              child: const Text('Login'),
-                            ),
-                            TextButton(
-                                onPressed: () {
-                                  Get.off(const SignUp());
-                                },
-                                child: const Text('Sign up here!'))
-                          ]))
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                      prefixIcon: Icon(Icons.email),
+                                      labelText: 'E-mail',
+                                      hintText: 'E-mail',
+                                      border: OutlineInputBorder()),
+                                  controller: authController.emailController,
+                                ),
+                                const SizedBox(height: 30),
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                      prefixIcon: Icon(Icons.fingerprint),
+                                      labelText: 'Password',
+                                      hintText: 'Password',
+                                      border: OutlineInputBorder(),
+                                      suffixIcon: IconButton(
+                                        onPressed: null,
+                                        icon: Icon(Icons.remove_red_eye_sharp),
+                                      )),
+                                  controller: authController.passwordController,
+                                ),
+                                const SizedBox(height: 30),
+                                const Align(
+                                    alignment: Alignment.centerRight,
+                                    child: TextButton(
+                                        onPressed: null,
+                                        child: Text('Forgot Password?'))),
+                                ElevatedButton(
+                                  //ADMIN AUTH
+                                  onPressed: () {
+                                    if (authController.authEmail == 'ad' &&
+                                        authController.authPassword == 'ad') {
+                                      authController.adminLogin();
+                                    }
+                                  },
+                                  child: const Text('Login'),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('New here?'),
+                                    TextButton(
+                                      onPressed: () {
+                                        Get.off(const SignUp());
+                                      },
+                                      child: Text(
+                                        'Sign Up here!',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]))
                     ])))),
     );
   }
